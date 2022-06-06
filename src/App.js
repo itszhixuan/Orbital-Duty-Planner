4dc8981964cce766ccbfb198bdd72ed11e82c3d3
Lee Zhi Xuan <leezhixuan001@gmail.com>
2022-06-06 18:11:57 +0800

    footer with link to github

--- src/App.js
+++ src/App.js
@@ -10,14 +10,22 @@ function App() {
     <Router>
       <div className="App">
         <main>
-          <div className="App-Intro">
+          <section className="App-Intro">
             <Navigation />
             <Routes>
               <Route path="/" element={<Homepage />}/>
               <Route path="/homepage" element={<Homepage />}/>
               <Route path="/login" element={<Login />}/>
             </Routes>
-          </div>
+          </section>
+          <section className="footer">
+            <h1>Read more at: </h1>
+            <div className="icons">
+              <a target="_blank" href="https://github.com/itszhixuan/Orbital-Duty-Planner">
+                <i class='fa fa-github fa-xl'></i>
+              </a>
+            </div>
+          </section>
         </main>
       </div>
     </Router>
