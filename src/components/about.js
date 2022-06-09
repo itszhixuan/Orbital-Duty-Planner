737f4cafd9f91246cd0c33dc2fef0218dce20d9a
Lee Zhi Xuan <leezhixuan001@gmail.com>
2022-06-09 21:51:15 +0800

    done with about page, except colour

--- src/components/about.js
+++ src/components/about.js
@@ -25,29 +25,49 @@ function About() {
                 </nav>
                 <h1>About Us</h1>
             </div>
-
-            <div className='about-content'>
-                <div className='row'>
-                    <div className='about-col-1'>
-                        <h1>Our goal</h1>
-                    </div>
-                    <div className='about-col-2'>
-                        <p>
-                            Have you ever experienced the difficulty of creating a schedule for an organisation/club that fully integrates the demands, preferences and availability of each employee/member? 
-                        </p>
-                        <p>
-                            Many of the planners would spend hours just to cater to everyone’s availability while having to fulfil their organisation's requirements, such as having a minimum number of shifts per month. 
-                        </p>
-                        <p>
-                            As the number of employees/members increase, the task at hand gets exponentially harder.
-                        </p>
-                        <p>
-                            At Plan-it!, we provide a platform for organisations to automatically and optimally plan and schedule duties and shift work based on preferences given by members of the organisation.
-                        </p>
+            <div className="outer">
+                <div className='about-content'>
+                    <div className='row'>
+                        <div className='about-col-1'>
+                            <h1>Our goal</h1>
+                        </div>
+                        <div className='about-col-2'>
+                            <p>
+                                Have you ever experienced the difficulty of creating a schedule for an organisation/club that fully integrates the demands, preferences and availability of each employee/member? 
+                            </p>
+                            <p>
+                                Many of the planners would spend hours just to cater to everyone’s availability while having to fulfil their organisation's requirements, such as having a minimum number of shifts per month. 
+                            </p>
+                            <p>
+                                As the number of employees/members increase, the task at hand gets exponentially harder.
+                            </p>
+                            <p>
+                                At Plan-it!, we provide a platform for organisations to automatically and optimally plan and schedule duties and shift work based on preferences given by members of the organisation.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
 
+            <div className="outer">
+            <div className="about-features">
+                <h1>Our Features</h1>
+                <div className="row">
+                    <div className="abtfeatures">
+                        <h3>Hassle-free creation of events</h3>
+                        <p>All you need to do is to create an event, select the restrictions and conditions that you desire and wait for your participants to respond!</p>
+                    </div>
+                    <div className="abtfeatures">
+                        <h3>Automatic schedule generator</h3>
+                        <p>Once all the inputs are in, a schedule catered to the availability of your participants can be generated with just one click of a button!</p>
+                    </div>
+                    <div className="abtfeatures">
+                        <h3>Swapping shifts</h3>
+                        <p>After the schedule is published, any internal shift swaps between your participants can be easily recorded by the organisation!</p>
+                    </div>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
