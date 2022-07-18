f2ab930f46de08be0557b3bb5e7b8d9cd2fc4583
Lee Zhi Xuan <leezhixuan001@gmail.com>
2022-07-18 23:11:36 +0800

    styling for about page, prevent overflow on smaller screens

--- src/components/about.js
+++ src/components/about.js
@@ -33,11 +33,9 @@ function About() {
             </div>
             <div className="outer">
                 <div className='about-content'>
-                    <div className='row'>
-                        <div className='about-col-1'>
-                            <h1>Our goal</h1>
-                        </div>
-                        <div className='about-col-2'>
+                    <h1>Our goal</h1>
+                    <div className='alignment'>
+                        <div className='about-col-final'>
                             <p>
                                 Have you ever experienced the difficulty of creating a schedule for an organisation/club that fully integrates the demands, preferences and availability of each employee/member? 
                             </p>
@@ -53,26 +51,23 @@ function About() {
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div className="outer">
-            <div className="about-features">
-                <h1>Our Features</h1>
-                <div className="row">
-                    <div className="abtfeatures">
-                        <h3>Hassle-free creation of events</h3>
-                        <p>All you need to do is to create an event, select the restrictions and conditions that you desire and wait for your participants to respond!</p>
-                    </div>
-                    <div className="abtfeatures">
-                        <h3>Automatic schedule generator</h3>
-                        <p>Once all the inputs are in, a schedule catered to the availability of your participants can be generated with just one click of a button!</p>
-                    </div>
-                    <div className="abtfeatures">
-                        <h3>Swapping shifts</h3>
-                        <p>After the schedule is published, any internal shift swaps between your participants can be easily recorded by the organisation!</p>
+                <div className="about-features">
+                    <h1>Our Features</h1>
+                    <div className="row">
+                        <div className="abtfeatures">
+                            <h3>Hassle-free creation of events</h3>
+                            <p>All you need to do is to create an event, select the restrictions and conditions that you desire and wait for your participants to respond!</p>
+                        </div>
+                        <div className="abtfeatures">
+                            <h3>Automatic schedule generator</h3>
+                            <p>Once all the inputs are in, a schedule catered to the availability of your participants can be generated with just one click of a button!</p>
+                        </div>
+                        <div className="abtfeatures">
+                            <h3>Swapping shifts</h3>
+                            <p>After the schedule is published, any internal shift swaps between your participants can be easily recorded by the organisation!</p>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     )
